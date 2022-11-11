0cee10dc5a3e8099ed8ef0070099b8447fc79963
qendresakodraliu <qendresakodraliu2@gmail.com>
2022-11-11 13:29:50 +0100

    Fix Footer content

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className='text-center p-3 bg-dark text-white'>
-            @2022 Qendresa Kodraliu Portofolio
+            @2022 Qendresa Kodraliu Portfolio
         </div>
     )
 }
